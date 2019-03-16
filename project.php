f9b05bebe4a38fe747224dbead6300486a5992e6
Vijay Kothari <36456740+Vijaykothari97@users.noreply.github.com>
2019-03-17 01:43:36 +0530

    Add files via upload

---
+++ project.php
@@ -0,0 +1,274 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css" >
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/0.4.2/sweet-alert.css">
+      <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+        
+      <title > Kothari Clinic | Home </title>
+      <link  rel ="shortcut icon" type="image/png" href="img1/sthe1.png" >
+
+  	  <link href="http://localhost/project/some.css" rel="stylesheet" type="text/css">
+     
+  </head>  
+  
+  <body>
+    
+    <!--- navbar--->
+    
+    <nav class="navbar navbar-expand-md navbar-light sticky-top " style= "background-color: #5a4f4f;">
+      <div class="container-fluid"> 
+        <img class ="rounded-circle text-center" class="navbar-brand" src="img1/sthe1.png" style="margin-right: 1em;width:4em;">
+        <a class = "navbar-brand" href="#" style="color:white;"> Kothari Clinic </a>
+        
+        <button class="navbar-toggler" type ="button"  data-toggle="collapse" data-target=".navbar-collapse"  style="">
+          <span class ="navbar-toggler-icon" style="color: red;"></span>
+        </button>
+        
+        <div id ="forHovering" class=" navbar-collapse collapse" class=".navbar-collapse">
+          <ul class=" navbar-nav ml-auto">
+            <li class="nav-item" >
+              <a href="#content5" data-toggle=" collapse" data-target=".navbar-collapse.show" class="nav-link" style="color:white;">Home</a></li>
+            <li class="nav-item">
+              <a href="#number5" data-toggle=" collapse" data-target=".navbar-collapse.show" class="nav-link" style="color:white;">Contact</a></li>
+             <li class="nav-item">
+              <a href="#address5" data-toggle=" collapse" data-target=".navbar-collapse.show" class="nav-link" style="color:white;">Address</a></li>
+              <li class="nav-item">
+              <a href="#discunt5" data-toggle=" collapse" data-target=".nav-collapse.show" class="nav-link" style="color:white;">Discount</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <br>
+  
+    <!--content--->
+   <div id="content5" >
+    <div class="container-fluid padding" >
+      <div class="row padding" >
+        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-5" style="float: left;">
+          <h1 class="text-center" id="forMobile2">Dr. Sharmila Kothari</h1>
+          <img id="imageformobile" class="rounded-circle" src="img/ma.png"  width="65%" style="margin-left: 3em;">
+        </div>
+        <br>
+        <br>
+        <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 " style="float: right;">
+          <h1 class="text-center" id="forMobile1" >About Me</h1>
+          <p class="lead text-center"  style="text-align: center;"id="forMobilepara"> Dr Sharmila Kothari has 20 years of experience in clinical practice. She was an ex housephysian of Bhatia General Hospital and Masina General Hospital. She is Managing Director at Kothari Clinic. 
+          </p>
+        </div>
+      </div>
+    </div> 
+ </div>
+   <!--number--->
+  <br>
+  <div id="number5">
+  <div  class="text-center">
+    <h2 id="forLine"><span>Make a Contact</span></h2>
+  </div>
+  <br>
+    <div class= "container-fluid padding" >
+      <div class="row padding" >
+        <div class="col-md-12 " >
+          
+         
+              <div class="active">
+            <a href="tel:9920280012" class="btn btn-default" id="spacing" id="forSomething" > <h4 class="text-center" id="again"style="color:white;"><span ><i class="fas fa-phone" id="phoneLogo"></i></span> 9920280012</h4></a>
+            </div>
+          </div>
+        </div>
+      </div>
+   </div>
+  <!--address and time--->
+    <br>
+  <br>
+  <br>
+  <div id="address5">
+  <div  class="text-center">
+    <h2 id="forLine"><span>Address and Timing</span></h2>
+  </div>
+  
+  <div class="text-center" id="someMoreSpacing">
+    <div class="container-fluid padding" >
+      <div class="row padding">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+          <div class="card" >
+            <div class="card-body text-center">
+              <h1 class="card-title" id="forMobile"><span  ><i class="fas fa-address-card"></i></span> Address</h1>
+              <p class="card-text" >10,Bharati Niwas <br>
+               Nr.Ashoka House,M.G.Rd,<br>
+               Kandivali (W),Mumbai-400067.
+              </p>
+            </div>
+          </div>
+        <br>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 ">
+          <div class="card " >
+            <div class="card-body text-center">
+              <h1 class="card-title" id="forMobile"><span><i class="far fa-clock"></i></span> Time</h1>
+              <p class="card-text" >11.00am-1.00pm <br>
+               8pm-10pm<br>
+               <br>
+              </p>
+            </div>
+          </div>
+        <br>
+        </div> 
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>
+ </div>
+  <div id="discunt5" >
+  <div  class="text-center">
+    <h2 id="forLine"><span>Avail For Discount</span></h2>
+  </div>
+  <br>
+   <div class= "container-fluid padding">
+    <div class="col-md-12">
+      <ul>
+        <li>Please press the button below </li>
+        <li>After pressing the button  a form will be appreared</li>
+        <li>Please fill all the information that is asked to be filled in the form</li>
+      </ul>
+      <br>
+   </div>
+ </div>
+</div>
+  <button type="button" class="btn btn-primary " id="Discountstyle" data-toggle="modal" data-target="#myModal">Click me</button>
+
+
+
+<!-- The Modal -->
+<div class="modal fade" id="myModal" >
+  <div class="modal-dialog modal-full">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header ">
+        <h4 class="modal-title"  id="forSomeCss">Kothari Clinic</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body" >
+        <div class="card" style="border-color: #3895D3;">
+          
+          
+             <h2 class="card-header card-styling" style="background-color: #3895D3;color: white;">Register</h2>
+          <div class="card-body " >
+           
+            <div class="card-text">
+             <form action="project2.php" method="POST" id="frmBox" onsubmit="return formSubmit();">
+               <div class="form-group">
+                 <label for="Name">Full Name:</label>
+                <input type="input" name="name" class="form-control"  placeholder="Kindly enter your name" style="border-radius: 1em;"><span value='<?php echo $namerr ;?>'></span>
+              </div>
+               <div class="form-group">
+                 <label for="Name">Contact Number:</label>
+                <input type="input" name="telephone" class="form-control"  placeholder="Kindly enter your valid number"  style="border-radius: 1em;"><span value = '<?php echo $tele ;?>' ></span>
+              </div>
+
+         
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="submit" name="submit" class="btn btn-primary" >Submit</button>
+        
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<footer class="deco">
+<hr class="my-4  ">
+<div class="container-fluid padding" >
+  <div class="row padding">
+<div class="col-12">
+
+
+  <p style="color: white;" class="text-center">Made with <span> <i id ="heart" class="fas fa-heart"></i></span>  by Vijay Kothari</p>
+</div>
+</div>
+</div>
+
+
+</footer>
+
+<!--
+  <div id="hmm" class="text-center "> <div class="alert alert-success alert-dismissible fade show text-center" id="designAlert">
+    <button type="button" class="close" data-dismiss="alert">&times;</button><br><br><br>
+      <h1 class="text-center"><i class="fas fa-check-square fa-2x" style="color: green;"></i></h1>
+   <h3 class="text-center">Submitted succesfully</h2>
+  <p class="text-center"> We will contact you shortly ,Thankyou !</p>
+ 
+    <button  type="button class="btn btn-primary close" data-dismiss="alert" >OK</button>
+  </div>
+  
+  
+ 
+
+  </div>
+
+-->
+  
+
+    <script  src="http://code.jquery.com/jquery-3.1.1.min.js" ></script>
+       <script type="text/javascript">
+  
+function formSubmit(){
+  $.ajax ({
+    type:'POST',
+    url:'project2.php',
+    data: $('#frmBox').serialize(),
+    success:function()
+    {
+      swal({ 
+  title: "Success",
+   text: "Thankyou for Registration! We will call you back ",
+    type: "success" 
+  },
+  function(){
+      window.location.href = 'project.php';
+});
+}
+});
+var form = document.getElementById('frmBox').reset();
+ return false;
+}
+
+</script>  
+  
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script  src="http://code.jquery.com/jquery-3.1.1.min.js" ></script>    
+
+          
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/0.4.2/sweet-alert.min.js"></script>
+  
+
+
+
+
+
+  </body>
+</html>
